535713ca24dc1b3aa4ff7bce375331bb2df21bd6
Lionel Lecaque <lionel@taotesting.com>
2015-07-28 17:15:54 +0200

    add binary and adapts tools to use it

--- bin/taoTools.sh
+++ bin/taoTools.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo toto
+if [ $# = 0 ];
+then
+        echo "usage: $0 TAOROOT CMD"
+        exit 1
+fi
+TAOROOT=$1
+CMD=$2
+vendor/bin/phing -f vendor/oat-sa/tao-build/tools.xml -Dtao.root=$TAOROOT $CMD
